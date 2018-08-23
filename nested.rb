a2f718924778a391fd7be52031d1ff0cd4ef2e9a
Olivier Willems <ojwillems@gmail.com>
2018-08-23 14:27:29 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -114,5 +114,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] = :languages.push("")
+  programmer_hash[:dennis_ritchie][:languages] = :languages.push("assembly")
 end
