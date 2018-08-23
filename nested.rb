ce69a3401a3ebee2b0bb3c15f16d3489b77c0c69
Olivier Willems <ojwillems@gmail.com>
2018-08-23 14:49:12 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -115,6 +115,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
+  binding.pry
   programmer_hash[:dennis_ritchie][:languages] = programmer_hash[:dennis_ritchie][:language].push("Assembly")
   programmer_hash
 end
